cb1d1a1802e2933c8a136a08ec6f8490e3f11c99
stayForward09 <daveescott0059@gmail.com>
2018-09-23 21:05:26 +0100

    :art: 404 image optimized and RhythmicExcellence logo in manifes

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'RhythmicExcellence',
+        short_name: 'RhythmicExcellence',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/logo.png', // This path is relative to the root of the site.
       },
     },
     {
