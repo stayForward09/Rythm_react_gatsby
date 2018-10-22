0a1431a2c22105815a1daf5e7872a71aa67c1ac4
AnichoKraus <anichokraus3123@gmail.com>
2018-10-22 22:10:37 +0100

    :bug: Fix client URL in config.js

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ const windowGlobal = typeof window !== 'undefined' && window;
 const hostname = windowGlobal && windowGlobal.location && windowGlobal.location.hostname;
 let backendHost;
 
-if (hostname === 'rhythmic-excellence.netlify.com') {
+if (hostname === 'rhythmicexcellence.london') {
   backendHost = 'https://sb59j69cub.execute-api.us-east-1.amazonaws.com/production';
 } else if (hostname === 'rhythmic-excellence-staging.netlify.com') {
   backendHost = 'https://sb59j69cub.execute-api.us-east-1.amazonaws.com/dev';
